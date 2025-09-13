40f4f0c51107f497175c13ee9edede364d6d4304
Phyo SiThu Htet <89130272+PhyoSiThuHtet@users.noreply.github.com>
2025-09-13 23:04:23 +0630

    Update vite.config.js second

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // root directory ကို သတ်မှတ်ပါ
+  root: '.',
+  build: {
+    // build output အတွက် directory သတ်မှတ်ပါ
+    outDir: 'dist',
+    rollupOptions: {
+      // input file ကို သတ်မှတ်ပါ
+      input: path.resolve(__dirname, 'public/index.html')
+    }
+  },
+  // Public directory ကို သတ်မှတ်ပါ
+  publicDir: 'public'
 });
